c12818d4ca06a78b9f9c3ffd71dc43387c1dacd4
Robert <karongorobert62@gmail.com>
2025-03-14 10:00:39 +0300

    learning python introduction

---
+++ intro.py
@@ -0,0 +1,3 @@
+print("Robert,welcome to Python")
+print(65)
+print(17)
---
+++ numbers.py
@@ -0,0 +1,13 @@
+print(10+15)
+print(10/5)
+print(10*5)
+print(10**2) #power 2
+print(10//2.5)
+print(10%2.5) #modulus===gets the reminder
+print(10,23,54,65,76,55,98)
+print(6,"Big","Bee")
+
+print(10>5)
+print(10<5)
+print(10==5)
+print(10>=5)
